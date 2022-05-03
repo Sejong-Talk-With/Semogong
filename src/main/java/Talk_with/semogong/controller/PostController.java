f7be4961436c629efee4827f0e74363096975dc2
bob8dod <bob8dod@gmail.com>
2022-05-04 00:38:10 +0900

    version 1.0.8, solve issue #2 #23

--- src/main/java/Talk_with/semogong/controller/PostController.java
+++ src/main/java/Talk_with/semogong/controller/PostController.java
@@ -9,6 +9,7 @@ import Talk_with.semogong.domain.form.PostEditForm;
 import Talk_with.semogong.service.MemberService;
 import Talk_with.semogong.service.PostService;
 import Talk_with.semogong.service.S3Service;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.commonmark.node.Node;
@@ -23,6 +24,14 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+import static java.time.format.FormatStyle.LONG;
 
 @Controller
 @RequiredArgsConstructor
@@ -47,6 +56,7 @@ public class PostController {
         }
         PostEditForm postEditForm = new PostEditForm(post);
         model.addAttribute("postForm", postEditForm);
+        model.addAttribute("check", true);
         return "post/editPostForm";
     }
 
@@ -64,18 +74,27 @@ public class PostController {
         }
         PostEditForm postEditForm = new PostEditForm(post);
         model.addAttribute("postForm", postEditForm);
+
         return "post/editPostForm :: #postEdit_container";
     }
 
     // 게시글 수정
     @PostMapping("/posts/edit/{id}")
-    public String edit(@PathVariable("id") Long id, @Valid @ModelAttribute("postForm") PostEditForm postEditForm, BindingResult result) {
-        if (result.hasErrors()) {
-            log.info("found Null, required re-post");
-            return "post/editPostForm"; }
+    public String edit(@PathVariable("id") Long id, @Valid @ModelAttribute("postForm") PostEditForm postEditForm, Model model, Authentication authentication) {
+//        if (result.hasErrors()) {
+//            log.info("found Null, required re-post");
+//            return "post/editPostForm"; }
         postEditForm.setHtml(markdownToHTML(postEditForm.getContent()));
         postService.edit(postEditForm);
-        return "redirect:/";
+
+        Member member = getLoginMember(authentication);
+
+        PostViewDto postViewDto = new PostViewDto(postService.findOne(id));
+        model.addAttribute("post", postViewDto);
+        model.addAttribute("member", member);
+        model.addAttribute("check", true);
+
+        return "components/postModal :: #postModal_content";
     }
 
 //    @PostMapping("/posts/edit/{id}")
@@ -89,12 +108,13 @@ public class PostController {
 //    }
 
     // 게시글 이미지 업로드 및 변경
+    @ResponseBody
     @PostMapping("/posts/edit/{id}/img")
-    public void postImgEdit(@PathVariable("id") Long id, @RequestParam("file") MultipartFile[] files) throws IOException {
-
+    public Image postImgEdit(@PathVariable("id") Long id, @RequestParam("file") MultipartFile[] files) throws IOException {
         MultipartFile file = files[0];
         Image image = s3Service.upload(file);
         postService.editPostImg(id, image);
+        return image;
     }
 
     // 게시글 삭제
@@ -130,5 +150,76 @@ public class PostController {
         HtmlRenderer renderer = HtmlRenderer.builder().build();
 
         return renderer.render(document);
+    }
+
+    @Data
+    static class PostViewDto {
+
+        // Post Info
+        private long id;
+        private String title;
+        private String introduce;
+        private String content;
+        private String html;
+        private LocalDateTime createTime;
+        private String formatCreateDate;
+        private List<String> times = new ArrayList<>();
+        private List<HomeController.CommentViewDto> comments = new ArrayList<>();
+        private int commentCount;
+        private StudyState state;
+        private Image postImg;
+        private Image memberImg;
+
+        // Member Info
+        private Long memberId;
+        private String memberName;
+        private String memberNickname;
+        private String memberDesiredJob;
+
+
+        public PostViewDto(Post post) {
+            this.id = post.getId();
+            this.title = post.getTitle();
+            this.introduce = post.getIntroduce();
+            this.content = post.getContent();
+            this.html = post.getHtml();
+            this.createTime = post.getCreateTime();
+            this.times = post.getTimes();
+            this.comments = post.getComments().stream().map(HomeController.CommentViewDto::new).collect(Collectors.toList());
+            this.commentCount = comments.size();
+            this.state = post.getState();
+            this.memberName = post.getMember().getName();
+            this.memberNickname = post.getMember().getNickname();
+            this.memberId = post.getMember().getId();
+            this.memberDesiredJob = post.getMember().getDesiredJob();
+            this.postImg = post.getImage();
+            this.memberImg = post.getMember().getImage();
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofLocalizedDate(LONG).withLocale(Locale.ENGLISH);
+            this.formatCreateDate = createTime.format(dateFormatter);
+        }
+
+    }
+
+    @Data
+    static class MemberDto {
+
+        // member info
+        private Long id;
+        private String name;
+        private Image image;
+        private StudyState state;
+
+        public MemberDto(Member member) {
+            this.id = member.getId();
+            this.name = member.getName();
+            this.image = member.getImage();
+            this.state = member.getState();
+        }
+    }
+
+    private Member getLoginMember(Authentication authentication) {
+        MyUserDetail userDetail = (MyUserDetail) authentication.getPrincipal();  //userDetail 객체를 가져옴 (로그인 되어 있는 놈)
+        String loginId = userDetail.getEmail();
+        return memberService.findByLoginId(loginId); // "박승일"로 로그인 했다고 가정, 해당 로그인된 회원의 ID를 가져옴
     }
 }
